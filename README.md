4d40008f63dcaec4cd0a636b7febdab0b99ae309
Helias <stefanoborzi32@gmail.com>
2015-11-22 15:21:47 +0100

    WoW Playermap

---
+++ README.md
@@ -0,0 +1,7 @@
+# AzerothShard Playermap
+
+## Configure
+
+Make a copy of config/playermap_config.php.conf and configure it.
+
+Open pomm_conf.php and set the realmd_id
---
+++ config/config-protected.php
@@ -0,0 +1,12 @@
+<?php
+$realmd = array(
+'db_type'         => 'mysql',
+'db_host'         => '127.0.0.1',
+'db_port'         => '3306',
+'db_username'     => 'root',
+'db_password'     => 'PASSWORD',
+'db_name'         => 'auth',
+'db_encoding'     => 'utf8',
+);
+$project = "trinity";
+?>
